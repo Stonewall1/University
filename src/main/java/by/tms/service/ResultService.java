15db0dd8a1a42d6ac90520a6945941d4d5102f47
Stonewall1 <impression97p@gmail.com>
2022-10-13 18:51:11 +0300

    MapFilling

--- src/main/java/by/tms/service/ResultService.java
+++ src/main/java/by/tms/service/ResultService.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class ResultService {
@@ -26,8 +24,20 @@ public class ResultService {
     @Transactional
     public Map<Subject, List<Integer>> performanceByStudent(Long id){
         Map<Subject , List<Integer>> map = new HashMap<>();
-        List<Result> resultsOfStudent = findAll().stream().filter(result -> result.getStudent().getId() == id).toList();
-        //toDo
+        List<Result> resultsOfStudent = findAll()
+                .stream()
+                .filter(result -> result.getStudent().getId() == id)
+                .toList();
+        Set<Subject> subjectSet = resultsOfStudent.get(0).getStudent().getSubjects();
+        for(Subject sub : subjectSet){
+            List<Integer> list = new ArrayList<>();
+            for(Result res : resultsOfStudent){
+                if(res.getSubject().equals(sub)){
+                    list.add(res.getPerformance());
+                }
+            }
+            map.put(sub , list);
+        }
         return map;
     }
 }
